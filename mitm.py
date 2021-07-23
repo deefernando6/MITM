f4b25793a035f42acb44f7c2ea517ca871132cb5
deeghayu <deefernando6@gmail.com>
2021-07-23 09:22:09 +0530

    Update mitm.py

--- mitm.py
+++ mitm.py
@@ -2,3 +2,8 @@ from scapy.all import *
 import threading
 import os
 import sys
+
+print("Are you run with root previledges? ")
+VIP = input("Enter the IP address of the victim: ")
+GW = input("Enter the IP address of the gateway: ")
+IFACE = input("Enter the name of the interface (ex: eth0): ")
